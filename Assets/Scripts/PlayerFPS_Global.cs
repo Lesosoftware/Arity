389425a4328d850dfdf4570eac0b44f2047530f2
Leso_KN <info@lesosoftware.com>
2015-12-22 15:55:47 +0100

    Medium Run #1 // Camera Rotation added, better player movement, AlphaZentauri: default sky added (not in use yet) // Build #1 was created

--- Assets/Scripts/PlayerFPS_Global.cs
+++ Assets/Scripts/PlayerFPS_Global.cs
@@ -9,37 +9,56 @@ public class PlayerFPS_Global : MonoBehaviour {
 	public int canJump = 0;
 	private Rigidbody rigidbody;
 
+	private Transform camera;
+	private enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+	private RotationAxes axes = RotationAxes.MouseXAndY;
+	public float CamSensitivityX = 8F;
+	public float CamSensitivityY = 8F;
+	private float MinCamX = -360F;
+	private float MaxCamX = 360F;
+	public float MinCamY = -70F;
+	public float MaxCamY = 60F;
+	float CamRotationY = 0F;
+
 	// Use this for initialization
 	void Start () {
 		rigidbody = GetComponent<Rigidbody>();
+		camera = GameObject.Find ("Cam_Main").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if ((rigidbody.velocity.magnitude < walkSpeed*5 && !Input.GetKey (KeyCode.LeftShift)) || (rigidbody.velocity.magnitude < runSpeed*5 && Input.GetKey (KeyCode.LeftShift))) {
+		if ((rigidbody.velocity.magnitude < walkSpeed * 5 && !Input.GetKey (KeyCode.LeftShift)) || (rigidbody.velocity.magnitude < runSpeed * 5 && Input.GetKey (KeyCode.LeftShift))) {
 			if (Input.GetKey (KeyCode.W)) {
 				// Forward
-				rigidbody.AddRelativeForce(new Vector3(0,0,walkAccel*40));
+				rigidbody.AddRelativeForce (new Vector3 (0, 0, walkAccel * 40));
 			}
 			if (Input.GetKey (KeyCode.S)) {
 				// Back
-				rigidbody.AddRelativeForce(new Vector3(0,0,-walkAccel*30));
+				rigidbody.AddRelativeForce (new Vector3 (0, 0, -walkAccel * 30));
 			}
 			if (Input.GetKey (KeyCode.A)) {
 				// Left
-				rigidbody.AddRelativeForce(new Vector3(-walkAccel*35,0,0));
+				rigidbody.AddRelativeForce (new Vector3 (-walkAccel * 35, 0, 0));
 			}
 			if (Input.GetKey (KeyCode.D)) {
 				// Right
-				rigidbody.AddRelativeForce(new Vector3(walkAccel*35,0,0));
+				rigidbody.AddRelativeForce (new Vector3 (walkAccel * 35, 0, 0));
 			}
 		}
 		if (Input.GetKey (KeyCode.Space)) {
 			// Jump
-			if (canJump>0) {
-				rigidbody.AddRelativeForce(new Vector3(0,jumpVel*200,0));
+			if (canJump > 0) {
+				rigidbody.AddRelativeForce (new Vector3 (0, jumpVel * 200, 0));
 			}
 		}
+		float rotationX = transform.localEulerAngles.y + Input.GetAxis ("Mouse X") * CamSensitivityX;
+		
+		CamRotationY += Input.GetAxis ("Mouse Y") * CamSensitivityY;
+		CamRotationY = Mathf.Clamp (CamRotationY, MinCamY, MaxCamY);
+		
+		camera.localEulerAngles = new Vector3 (-CamRotationY, 0, 0);
+		transform.Rotate (0, Input.GetAxis ("Mouse X") * CamSensitivityX, 0);
 	}
 
 	void OnCollisionEnter(Collision col)
