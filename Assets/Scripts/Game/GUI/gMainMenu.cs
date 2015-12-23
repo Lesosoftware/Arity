04dcb4c075d21422b65e632260758a68bd74d212
Sewbacca <sewbacca@live.de>
2015-12-23 22:16:23 +0100

    GUI // Initail Commit // Merge

---
+++ Assets/Scripts/Game/GUI/gMainMenu.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gMainMenu : MonoBehaviour
+{
+	public Object settingsMenu;
+	int screenWidth = Screen.width;
+	int screenHeight = Screen.height;
+
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+	
+	void OnGUI()
+	{
+		int groupWidth = 500;
+		int groupHeight = 452;
+		
+		int groupX = ( screenWidth - groupWidth ) / 2;
+		int groupY = ( screenHeight - groupHeight ) / 2;
+		
+		GUI.BeginGroup( new Rect( groupX, groupY, groupWidth, groupHeight ) );
+		GUI.Box( new Rect( 0, 0, groupWidth, groupHeight ), "Pause" );
+		
+		if ( GUI.Button( new Rect( 23, 71, 452, 71), "Zurück zum Spiel" ) )
+		{
+			Destroy(gameObject);
+		}
+		if ( GUI.Button( new Rect( 23, 166, 452, 71 ), "Einstellungen" ) )
+		{
+			Instantiate(settingsMenu);
+			Destroy(gameObject);
+		}
+
+		if ( GUI.Button( new Rect( 23, 261, 452, 71), "Speichern und beenden" ) ) { }
+
+		if ( GUI.Button( new Rect( 23, 357, 452, 71 ), "Speichern und Spiel beenden" ) )
+		{
+			Application.Quit();
+		}
+
+		GUI.EndGroup();
+	}
+}
---
+++ Assets/Scripts/Game/GUI/gSettingsMenu.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gSettingsMenu : MonoBehaviour
+{
+	public Object mainMenu;
+	int screenWidth = Screen.width;
+	int screenHeight = Screen.height;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnGUI()
+	{
+		int settingsGroupWidth = 500;
+		int settingsGroupHeight = 452;
+		
+		int settingsGroupX = (screenWidth - settingsGroupWidth) / 2;
+		int settingsGroupY = (screenHeight - settingsGroupHeight) / 2;
+		
+		GUI.BeginGroup (new Rect (settingsGroupX, settingsGroupY, settingsGroupWidth, settingsGroupHeight));
+		GUI.Box (new Rect (0, 0, settingsGroupWidth, settingsGroupHeight), "Einstellungen");
+
+		if (GUI.Button (new Rect (23, 71, 452, 71), "Grafik"))
+		{
+			Destroy (gameObject);
+		}
+
+		if (GUI.Button (new Rect (23, 166, 452, 71), "Audio"))
+		{
+			Application.LoadLevel (2);
+		}
+
+		if (GUI.Button (new Rect (23, 261, 452, 71), "Steuerung"))
+		{
+			Application.LoadLevel (3);
+		}
+		
+		if (GUI.Button (new Rect (23, 357, 452, 71), "Zurück"))
+		{
+			Instantiate(mainMenu);
+			Destroy (gameObject);
+		}
+		
+		GUI.EndGroup ();
+	}
+}
